08e7a9f000075e806c38121938861b86f654c210
Znis Twayana <metwayana@gmail.com>
2024-06-04 02:07:55 +0545

    feature: light and dark theme toggle

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+const body = document.querySelector("body");
+
+const themeToggleBtn = document.getElementById("theme-toggle");
+const gradientEffect = document.getElementById("gradient-effect");
+const cartBtn = document.getElementById("navbar__cart-icon");
+const searchBtn = document.getElementById("navbar__search-icon");
+const arrowBtn = document.getElementById("hero__arrow-icon");
+
+
+
+
+
+function themeToggle(){
+    const popularTrendsCard = document.querySelectorAll(".popular-trends-card");
+    if(!body.classList.length){
+        cartBtn.src = "./images/cart-icon-white.svg";
+        searchBtn.src = "./images/search-icon-white.svg";
+        arrowBtn.src = "./images/arrow-bottom-icon-white.svg";
+        gradientEffect.style.display = "block";
+
+        popularTrendsCard.forEach(element => {
+            element.style.background = "linear-gradient(163.76deg, rgba(255, 255, 255, 0.7) 0%, rgba(255, 255, 255, 0.1) 100.82%)";
+        
+          });
+          
+        body.classList.toggle("dark-mode");
+    } else {
+        cartBtn.src = "./images/cart-icon-black.svg";
+        searchBtn.src = "./images/search-icon-black.svg";
+        arrowBtn.src = "./images/arrow-bottom-icon-black.svg";
+        gradientEffect.style.display = "none";
+
+        popularTrendsCard.forEach(element => {
+            element.style.background = "#282828";
+        
+          });
+
+        body.classList.toggle("dark-mode");
+    }
+}
+themeToggleBtn.onclick = themeToggle;
+
